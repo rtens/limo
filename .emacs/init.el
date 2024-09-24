7c5e66ab7ea268944033d6223ba341717ecbda68
Nikolas Martens <Nikolas.M@rtens.org>
2024-09-24 20:21:05 +0200

    Run tests in shell on windows

--- .emacs/init.el
+++ .emacs/init.el
@@ -3,19 +3,31 @@
 	(interactive)
 	(neotree-hide)
 	(delete-other-windows)
-	(split-window-horizontally -20)
+	(split-window-horizontally -25)
 	(other-window 1)
 	(switch-to-buffer "limo test watch")
 	(other-window 1))
 
 (global-set-key (kbd "C-x 9") 'my-limo-restore-windows)
 
+(defun my-shell (command)
+	"Executes the command in a new shell"
+	(interactive "P")
+	(if (eq system-type 'windows-nt)
+			(progn
+				(shell)
+				(switch-to-buffer "*shell*")
+				(goto-char (point-max))
+				(insert command)
+				(comint-send-input nil t))
+		(term command)))
+
 (unless (get-buffer "limo test watch")
 	(delete-other-windows)
 	(split-window-horizontally -20)
 	(other-window 1)
 	(cd (projectile-project-root))
-	(term "npx ava --watch")
+	(my-shell "npx ava --watch")
 	(rename-buffer "limo test watch")
 	(other-window 1))
 
